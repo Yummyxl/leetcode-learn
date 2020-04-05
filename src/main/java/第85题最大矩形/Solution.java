e18594519657037a4a278d1e0ecf884e27981085
Yummyxl <2548341581@qq.com>
2020-04-05 17:15:15 +0800

    第85题最大矩形

---
+++ src/main/java/第85题最大矩形/Solution.java
@@ -0,0 +1,73 @@
+package 第85题最大矩形;
+
+import java.util.Stack;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ *
+ * @Author: Yummyxl
+ * @Date: 2020/4/5
+ *
+ * 给定一个仅包含 0 和 1 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+ *
+ * 示例:
+ *
+ * 输入:
+ * [
+ *   ["1","0","1","0","0"],
+ *   ["1","0","1","1","1"],
+ *   ["1","1","1","1","1"],
+ *   ["1","0","0","1","0"]
+ * ]
+ * 输出: 6
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximal-rectangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+
+public class Solution {
+
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        int max = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[] heights = new int[n];
+
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
+                if (matrix[i][j] == '1') {
+                    heights[j]++;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+            max = Math.max(max, getMax(heights));
+        }
+        return max;
+    }
+
+    public int getMax(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+        int max = 0;
+
+        for(int i=0; i<heights.length; i++) {
+            while (stack.peek() != -1 && heights[stack.peek()] >= heights[i]) {
+                max = Math.max(max, heights[stack.pop()] * (i - stack.peek() - 1));
+            }
+            stack.push(i);
+        }
+
+        while (stack.peek() != -1) {
+            max = Math.max(max, heights[stack.pop()] * (heights.length - stack.peek() - 1));
+        }
+        return max;
+    }
+
+}
