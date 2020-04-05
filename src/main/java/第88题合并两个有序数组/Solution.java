51387da2b6ca99ad66d21a975c067dffc42125af
Yummyxl <2548341581@qq.com>
2020-04-05 18:42:27 +0800

    第88题合并两个有序数组

---
+++ src/main/java/第88题合并两个有序数组/Solution.java
@@ -0,0 +1,55 @@
+package 第88题合并两个有序数组;
+
+/**
+ * 应用模块名称<p>
+ * 代码描述<p>
+ *
+ * @Author: Yummyxl
+ * @Date: 2020/4/5
+ *
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 num1 成为一个有序数组。
+ *
+ *  
+ *
+ * 说明:
+ *
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。
+ * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ *  
+ *
+ * 示例:
+ *
+ * 输入:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ *
+ * 输出: [1,2,2,3,5,6]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int len1 = m - 1;
+        int len2 = n - 1;
+
+        int index = m + n -1;
+
+        while (len1 >= 0 && len2 >= 0) {
+            if (nums1[len1] > nums2[len2]) {
+                nums1[index--] = nums1[len1];
+                len1--;
+            } else {
+                nums1[index--] = nums2[len2];
+                len2--;
+            }
+        }
+
+        while (len2 >= 0) {
+            nums1[index--] = nums2[len2--];
+        }
+    }
+}
